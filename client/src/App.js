def732a7494d9277f4a316d5c26bb08b33f52f8d
tarekhassan410 <tarekhassan410@gmail.com>
2018-12-31 03:36:33 +0200

    React Router added and Semantic UI added

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,30 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import "./App.css";
+import Orders from "./components/orders/orders.js";
+import ViewOrder from "./components/orders/view_order.js";
+import { Header, Segment } from "semantic-ui-react";
+import "semantic-ui-css/semantic.css";
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <Router>
+        <div>
+          <Segment>
+            <Header as='h3'>
+              {" "}
+              <Link to="/orders"> Orders </Link>{" "}
+            
+              {" "} ||
+              <Link to="/view_order"> View single order </Link>{" "}
+            </Header>
+          </Segment>
+          <Route path="/orders" component={Orders} />
+          <Route path="/view_order" component={ViewOrder} />
+        </div>
+      </Router>
     );
   }
 }
