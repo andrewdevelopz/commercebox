28943ae41249130d28752acaa1248660c62f25d7
akeon-lee <akeon.lee@gmail.com>
2019-04-13 17:47:50 -0700

    distribute script created to work for cross platforms

---
+++ desktop/app/src/scripts/distribute.ts
@@ -0,0 +1,50 @@
+import fs, { Stats } from 'fs';
+import path from 'path';
+
+/**
+ *  This script will find all files that are not .ts or .tsx files in the /src directory
+ *  and copy them over to the /dist directory.
+ * 
+ *  @param start: Which directory it should start looking from
+ *  @param filters: The type of files that we are looking for
+ */
+
+// The arguments to be passed into distributeToDist()
+const src: string = './src', filetype: string[] = ['.html', '.css'];
+
+const distributeToDist = (start: string, filters: string[]): void => {
+    const files: string[] = fs.readdirSync(start);
+
+    // loop through all the files in directory
+    for (const file of files) {
+        const filename: string = path.join(start, file);
+        const stat: Stats = fs.lstatSync(filename);
+
+        // if the file is a directory we recurse to go in deeper
+        if (stat.isDirectory()) {
+            distributeToDist(filename, filters);
+        } else {
+            // loop through the filters and if the file matches the filter we get the result
+            for (const filter of filters) {
+                if (filename.indexOf(filter) >= 0) {
+                    // copy the found file to it's respected directory in /dist.
+                    //  - destination: the full path including the file
+                    //  - destArray: the full path including the file in array form
+                    //  - popped: the last item removed from destArray
+                    //  - afterPopped: the directory path after filename has been removed
+                    const destination: string = filename.replace('src', './dist');
+                    const destArray: string[] = destination.split('/');
+                    const popped: string | undefined = destArray.pop();
+                    const afterPopped: string = destArray.join('/');
+
+                    // if the directory does not exists, create it
+                    if (!fs.existsSync(afterPopped)) {
+                        fs.mkdirSync(afterPopped, { recursive: true });
+                    }
+                    fs.copyFileSync(filename, destination);
+                }
+            }
+        }
+    }
+}
+distributeToDist(src, filetype); // initiate the function
